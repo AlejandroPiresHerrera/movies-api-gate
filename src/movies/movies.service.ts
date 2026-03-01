4aa1778fda1f0cb0774f81a5ae2298108664330f
AlejandroPiresHerrera <alejandropires478@gmail.com>
2026-03-01 16:36:26 +0100

    test: introduce lint error (unused variable) - intentional fail

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -11,6 +11,7 @@ export class MoviesService {
   ) {}
 
   async listMovies(): Promise<Movie[]> {
+    const unusedVariable = 'esto va a fallar el lint'; // ❌ Fallo lint intencional
     return this.movieRepo.find({ order: { id: 'ASC' } });
   }
 }
